2aefbbc6a940ce31e5d28de4665002f4718a80cc
KIn <putthipat05@gmail.com>
2026-08-25 15:29:12 +0800

    Update SQLquery1.sql

--- SQLquery1.sql
+++ SQLquery1.sql
@@ -32,3 +32,30 @@ round((current_year_student - previous_year_student)/ nullif(previous_year_stude
 from yearly_sale 
 order by country_name asc,
 		 year asc
+
+--2.Dynamic Market Share & Regional Faculty Ranking and find top faculty in their region with calculate market share--
+with top_region as (
+select 
+a.region,
+c.faculty_name,
+sum(b.student_count) as top_student,
+dense_rank() over(partition by a.region order by sum(b.student_count) desc) as faculty_rank
+from dim_countries as a
+inner join fact_student_enrollments as b
+on a.country_id = b.country_id
+inner join dim_faculties as c
+on b.faculty_id = c.faculty_id
+where b.year = 2025
+group by a.region ,
+		 c.faculty_name 
+)
+
+select
+region,
+faculty_name,
+top_student,
+faculty_rank
+from top_region
+where faculty_rank <= 3
+order by region asc,
+         faculty_rank asc;
